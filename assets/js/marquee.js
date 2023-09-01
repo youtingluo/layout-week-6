146394577d5ca1eb1737076cca8590b093be5fd1
Youting <boy80314@gmail.com>
2023-09-01 17:29:40 +0800

    新增modal 與修改字體

--- assets/js/marquee.js
+++ assets/js/marquee.js
@@ -1,11 +1,15 @@
 import marquee from '../../node_modules/vanilla-marquee/dist/vanilla-marquee.min.js';
 export const myScroller = new marquee(document.getElementById("marquee1"), {
   direction: 'right',
+  duplicated: true,
+  gap: 0,
   recalcResize: true,
-  duration: 8000,
+  duration: 4000,
 })
 export const myScroller2 = new marquee(document.getElementById("marquee2"), {
   direction: 'left',
+  duplicated: true,
+  gap: 0,
   recalcResize: true,
-  duration: 8000,
+  duration: 4000,
 })
